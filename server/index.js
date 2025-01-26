12ffa1a8b904c415157b60bdb791deb10867dab4
kp4nk4j <pankajkvs2001@gmail.com>
2025-01-26 12:31:43 +0530

    Allow all origins

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors({ origin: 'https://insta-post-client.vercel.app' }));
+app.use(cors());
 app.use('/posts', postRoutes)
 const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
