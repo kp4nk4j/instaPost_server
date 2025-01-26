0b9377b3bcf58bb100a597e11901a93302f21af8
kp4nk4j <pankajkvs2001@gmail.com>
2025-01-26 11:46:22 +0530

    allow origin

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+app.use(cors({ origin: 'https://instapost-server.up.railway.app' }));
 app.use('/posts', postRoutes)
 const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
